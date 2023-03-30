f618a3f6092ad0f3791887174fe6309df35f406c
Chris Bowers <pcbowers@liberty.edu>
2023-03-30 15:01:08 -0400

    support async

--- public/attemptRickroll.js
+++ public/attemptRickroll.js
@@ -1,4 +1,4 @@
-setTimeout(() => {
-  window.umami.trackEvent("rickroll-succeeded", { type: "rickroll" });
+setTimeout(async () => {
+  await window.umami.trackEvent("rickroll-succeeded", { type: "rickroll" });
   window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 }, 50);
