fcd83375243039c0f865cad23a967602046cdc59
Chris Bowers <pcbowers@liberty.edu>
2023-03-30 13:45:32 -0400

    Improve Scripts

---
+++ avoided.js
@@ -0,0 +1,1 @@
+window.umami.trackEvent("rickroll-avoided", { type: "rickroll" });
--- main.js
+++ main.js
@@ -1,10 +1,2 @@
-function rickroll() {
-  window.umami.trackEvent("rickroll-succeeded", { type: "rickroll" });
-  window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-}
-
-rickroll();
-
-setTimeout(() => {
-  window.umami.trackEvent("rickroll-avoided", { type: "rickroll" });
-}, 1000);
+window.umami.trackEvent("rickroll-succeeded", { type: "rickroll" });
+window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
