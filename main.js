b83271bc040b30a8b936839f2606bd679d33a9d5
Chris Bowers <pcbowers@liberty.edu>
2023-03-30 13:33:08 -0400

    Removed do-not-track and cleaned up script

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
-let avoidedRickRoll = true;
-
-setTimeout(() => {
-  avoidedRickRoll = false;
+function rickroll() {
   window.umami.trackEvent("rickroll-succeeded", { type: "rickroll" });
-  console.log("You've been Rickrolled!");
   window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-}, 0);
+}
+
+rickroll();
 
 setTimeout(() => {
-  if (avoidedRickRoll) {
-    window.umami.trackEvent("rickroll-avoided", { type: "rickroll" });
-    console.log("You avoided being Rickrolled!");
-  }
+  window.umami.trackEvent("rickroll-avoided", { type: "rickroll" });
 }, 1000);
